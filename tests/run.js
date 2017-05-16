01662f13cd0ec1102b25b1f8c13873926d796590
Florian Simon <florian@tentwentyfour.lu>
2017-05-16 10:34:33 +0200

    Added a test script

---
+++ tests/run.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+"use strict";
+
+const chalk = require("chalk");
+
+let errors = require("./methodify-tests");
+
+if (errors.length) {
+    errors.forEach(error => {
+        console.log(chalk.red(`✘ ${error}`));
+    });
+
+    process.exit(1);
+}
+
+console.log(chalk.green(`✔ All test passed!`));
